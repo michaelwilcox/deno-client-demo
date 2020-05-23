66d60caa007182337098f12ddb64cff4b0b709a1
Mike Wilcox <mwilcox56@gmail.com>
2020-05-22 21:06:54 -0400

    swap f/e

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
-import { h } from 'preact';
-import logo from './logo.png';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className={'App'}>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://preactjs.com"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Preact
+          Learn React
         </a>
       </header>
     </div>
