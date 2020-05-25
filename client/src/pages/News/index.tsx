408f7ff6ce5b686a4a2aee0fdf514e57ef318b04
Mike Wilcox <mwilcox56@gmail.com>
2020-05-25 01:27:40 -0400

    add News page

---
+++ client/src/pages/News/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { dateToHours } from '../../utility/dates';
+import "./style.css";
+import { LatestNewsArticle } from "../../types";
+
+interface Props { }
+
+export default function News(props: Props) {
+  const [rss, setRSS] = useState({ items: [] });
+
+  async function fetchRSS() {
+    await fetch(`${process.env.REACT_APP_SERVER}/rss`)
+      .then((res) => res.json())
+      .then((res) => setRSS(res))
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    fetchRSS();
+  }, []); // empty array 2nd arg prevents re-fetching!
+
+  console.log(rss);
+
+  return (
+    <div id="news">
+      <div className="news-container container">
+        {rss.items.map((item: LatestNewsArticle) => (
+          <div className="news-row row">
+            <a href={item.link} rel="noopener" target="_blank">{item.title}</a>
+            <div><p>{item.sourceName}</p> <p>・</p> <p>{dateToHours(item.pubDate)}</p></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
