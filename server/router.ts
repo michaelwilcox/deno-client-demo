dbe367e73e2e8b59b4b9bcb414d78d718fc15dc0
Mike Wilcox <mwilcox56@gmail.com>
2020-05-25 14:05:48 -0400

    fixing route static content loading issue

--- server/router.ts
+++ server/router.ts
@@ -1,14 +1,15 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 
-import landing from './handlers/landing.ts';
-import symbol from './handlers/symbol.ts';
-import rss from './handlers/rss.ts';
+import staticPage from "./handlers/static.ts";
+import symbol from "./handlers/symbol.ts";
+import rss from "./handlers/rss.ts";
 
 const router = new Router();
 
 router
-    .get("/", landing)
-    .get("/symbol/:symbol", symbol)
-    .get("/rss", rss);
+  .get("/", staticPage)
+  .get("/chart/:symbol", staticPage)
+  .get("/symbol/:symbol", symbol)
+  .get("/rss", rss);
 
 export default router;
