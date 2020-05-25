e2551bbcacf7d86cae1bf8b52f15ba7ae7d6a406
Mike Wilcox <mwilcox56@gmail.com>
2020-05-25 01:30:54 -0400

    update rss handler

--- server/config.ts
+++ server/config.ts
@@ -1,10 +1,7 @@
 export default {
-    HOST: '127.0.0.1',
-    PORT: 8000,
-    IEX_TOKEN: 'pk_fafaf668b86d4a66b03fc8040fcdfaab',
-    IEX_API_BASE: 'https://cloud.iexapis.com/stable',
-    RSS_FEEDS: [
-        { name: 'Business Insider', url: 'https://markets.businessinsider.com/rss/news' },
-        { name: 'Seeking Alpha', url: 'https://seekingalpha.com/feed.xml' }
-    ]
-}
+  HOST: "127.0.0.1",
+  PORT: 8000,
+  IEX_TOKEN: "pk_fafaf668b86d4a66b03fc8040fcdfaab",
+  IEX_API_BASE: "https://cloud.iexapis.com/stable",
+  KOYFIN_BASE: "https://api.koyfin.com/api/v2/commands/n",
+};
--- server/handlers/rss.ts
+++ server/handlers/rss.ts
@@ -1,22 +1,19 @@
-import { RouterContext, Status } from 'https://deno.land/x/oak/mod.ts';
-import config from '../config.ts';
+import { RouterContext, Status } from "https://deno.land/x/oak/mod.ts";
+import config from "../config.ts";
 
 export default async (context: RouterContext) => {
-    const { params, response } = context;
-    const { RSS_FEEDS } = config;
-    try {
-        let promises = [];
-        for (let feed of RSS_FEEDS) {
-            promises.push(fetch(feed.url).then(res => res.text()));
-            await Promise.all(promises)
-                .then(res => {
-                    response.status = Status.OK;
-                    response.body = res;
-                });
-        }
-    } catch (e) {
-        console.error(e);
-        response.status = Status.BadRequest;
-        response.body = 'Bad Request';
-    }
-}
+  const { response } = context;
+  const { KOYFIN_BASE } = config;
+  try {
+    await fetch(`${KOYFIN_BASE}?limit=100&skip=0`)
+      .then((res) => res.json())
+      .then((res) => {
+        response.status = Status.OK;
+        response.body = res;
+      });
+  } catch (e) {
+    console.error(e);
+    response.status = Status.BadRequest;
+    response.body = "Bad Request";
+  }
+};
