23684e027d564b307fadf9f1119df932e19fc147
Mike Wilcox <mwilcox56@gmail.com>
2020-05-25 20:51:08 -0400

    fix load by url

--- server/handlers/static.ts
+++ server/handlers/static.ts
@@ -1,7 +1,13 @@
 import { RouterContext, send } from "https://deno.land/x/oak/mod.ts";
 
 export default async (context: RouterContext) => {
-  await send(context, context.request.url.pathname, {
+  // so, this is kind of a hack ...
+  // usecase here is need `/endpoint/:id`.
+  // app is doing data fetching on client
+  // so just serve always serve from root
+  // and let ui construct state from url
+  const pathName = "/";
+  await send(context, pathName, {
     root: `${Deno.cwd()}/client/build`,
     index: "index.html",
   });
