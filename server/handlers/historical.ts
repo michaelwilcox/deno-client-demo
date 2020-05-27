5200453a83d05f655021fcf01a6cedf51e0de0cb
Mike Wilcox <mwilcox56@gmail.com>
2020-05-27 08:20:37 -0400

    add charts data endpoint

---
+++ server/handlers/historical.ts
@@ -0,0 +1,31 @@
+import { RouterContext, Status } from "https://deno.land/x/oak/mod.ts";
+import config from "../config.ts";
+
+export default async (context: RouterContext) => {
+  const { response, params } = context;
+  if (!params.symbol) {
+    response.status = Status.BadRequest;
+    response.body = "Bad Request";
+    return;
+  }
+  const ranges = ["dynamic", "5d", "1mm", "1y", "5y"];
+  const { IEX_API_BASE, IEX_TOKEN } = config;
+  let promises = [];
+  try {
+    for (let range of ranges) {
+      promises.push(
+        fetch(
+          `${IEX_API_BASE}/stock/${params.symbol}/chart/${range}?token=${IEX_TOKEN}`
+        ).then((r) => r.json())
+      );
+    }
+    await Promise.all(promises).then((res) => {
+      response.status = Status.OK;
+      response.body = res;
+    });
+  } catch (e) {
+    console.error(e);
+    response.status = Status.BadRequest;
+    response.body = "Bad Request";
+  }
+};
--- server/router.ts
+++ server/router.ts
@@ -3,13 +3,18 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 import staticPage from "./handlers/static.ts";
 import symbol from "./handlers/symbol.ts";
 import rss from "./handlers/rss.ts";
+import historical from "./handlers/historical.ts";
 
 const router = new Router();
 
 router
+  // UI
   .get("/", staticPage)
   .get("/chart/:symbol", staticPage)
+
+  // API
   .get("/symbol/:symbol", symbol)
+  .get("/charts/:symbol", historical)
   .get("/rss", rss);
 
 export default router;
